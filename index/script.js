8743f4019f34738ed620fda61262a1e207df704d
skulito <fox04012007@gmail.com>
2021-02-06 13:28:42 -0300

    Enter

--- index/script.js
+++ index/script.js
@@ -4,6 +4,8 @@ let imgQuadrado = document.createElement('img')
 let imgRetangulo = document.createElement('img')
 let imgTriangulo = document.createElement('img')
 let imgLosango = document.createElement('img')
+let imgTrapezio = document.createElement('img')
+let imgCirculo = document.createElement('img')
 
 let check = document.querySelector('#check')
 
@@ -14,6 +16,10 @@ function quadrado() {
     imgTriangulo.style.display = 'none'
 
     imgLosango.style.display = 'none'
+
+    imgTrapezio.style.display = 'none'
+
+    imgCirculo.style.display = 'none'
 
     imgQuadrado.setAttribute('src', 'Imagens/quadrado.png')
     imgQuadrado.style.display = 'block'
@@ -44,6 +50,10 @@ function retangulo() {
     imgTriangulo.style.display = 'none'
 
     imgLosango.style.display = 'none'
+
+    imgTrapezio.style.display = 'none'
+
+    imgCirculo.style.display = 'none'
     
     imgRetangulo.setAttribute('src', 'Imagens/retangulo.png')
     imgRetangulo.style.display = 'block'
@@ -75,6 +85,10 @@ function triangulo() {
 
     imgLosango.style.display = 'none'
 
+    imgTrapezio.style.display = 'none'
+
+    imgCirculo.style.display = 'none'
+
     imgTriangulo.setAttribute('src', 'Imagens/triangulo.png')
     imgTriangulo.style.display = 'block'
 
@@ -105,6 +119,10 @@ function losango() {
 
     imgTriangulo.style.display = 'none'
 
+    imgTrapezio.style.display = 'none'
+
+    imgCirculo.style.display = 'none'
+
     imgLosango.setAttribute('src', 'Imagens/losango.png')
     imgLosango.style.display = 'block'
 
@@ -124,5 +142,67 @@ function losango() {
         imgLosango.style.animation = '360s rotateForma infinite'
 
     }, 500)
+
+}
+
+function trapezio() {
+
+    imgQuadrado.style.display = 'none'
+
+    imgRetangulo.style.display = 'none'
+
+    imgTriangulo.style.display = 'none'
+
+    imgLosango.style.display = 'none'
+
+    imgCirculo.style.display = 'none'
+
+    imgTrapezio.setAttribute('src', 'Imagens/trapezio.png')
+    imgTrapezio.style.display = 'block'
+
+    forma.appendChild(imgTrapezio)
+    forma.style.animation = '500ms increaseOut'
+
+    imgTrapezio.style.width = '350px'
+    imgTrapezio.style.marginTop = '-200px'
+    imgTrapezio.style.marginLeft = '575px'
+
+    check.style.marginTop = '280px'
+    check.style.opacity = '1'
+    check.style.transition = '250ms'
+
+    setTimeout(function () {
+
+        imgTrapezio.style.animation = '360s rotateForma infinite'
+
+    }, 500)
+
+}
+
+function circulo() {
+
+    imgQuadrado.style.display = 'none'
+
+    imgRetangulo.style.display = 'none'
+
+    imgTriangulo.style.display = 'none'
+
+    imgLosango.style.display = 'none'
+
+    imgTrapezio.style.display = 'none'
+
+    imgCirculo.setAttribute('src', 'Imagens/circulo.png')
+    imgCirculo.style.display = 'block'
+
+    forma.appendChild(imgCirculo)
+    forma.style.animation = '500ms increaseOut'
+
+    imgCirculo.style.width = '300px'
+    imgCirculo.style.marginTop = '-250px'
+    imgCirculo.style.marginLeft = '600px'
+
+    check.style.marginTop = '348px'
+    check.style.opacity = '1'
+    check.style.transition = '250ms'
 
 }
